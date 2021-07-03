e5206dc6f3a3420add44162935bd9d9282243607
keiinuki <85820876+keiinuki@users.noreply.github.com>
2021-07-03 11:42:54 +0900

    TODO実装から取り消し線表現まで

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 const addTaskTrigger = document.getElementsByClassName("js-addTask-trigger")[0];
 const addTaskTarget = document.getElementsByClassName("js-addTask-target")[0];
 const addTaskValue = document.getElementsByClassName("js-addTask-value")[0];
@@ -7,15 +9,25 @@ const removeTask = (removeButton) => {
   addTaskTarget.removeChild(targetTask);
 };
 
+const completeTask = (completeButton) => {
+  const targetTask = completeButton.closest("li");
+  targetTask.classList.add("isComplete");
+  targetTask.removeChild(completeButton);
+};
+
 const addTask = (task) => {
   const listItem = document.createElement("li");
   const removeButton = document.createElement("button");
+  const completeButton = document.createElement("button");
 
-  removeButton.innerText = "削除";
+  removeButton.innerText = "消すよ！";
   removeButton.addEventListener("click", () => removeTask(removeButton));
 
-  listItem.innerText = task;
+  completeButton.innerText = "やったよ！";
+  completeButton.addEventListener("click", () => completeTask(completeButton));
 
+  listItem.innerText = task;
+  listItem.append(completeButton);
   listItem.append(removeButton);
   addTaskTarget.appendChild(listItem);
 };
