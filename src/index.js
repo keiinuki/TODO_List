09498022c15e6163e3d0203550346a54ed92e25a
keiinuki <85820876+keiinuki@users.noreply.github.com>
2021-07-04 21:22:24 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import "./styles.css";
 
+// タスクを表示させるためのきっかけ(trigger)表示場所(target)値(value)を定義
 const addTaskTrigger = document.getElementsByClassName("addTask-trigger")[0];
 const addTaskTarget = document.getElementsByClassName("addTask-target")[0];
 const addTaskValue = document.getElementsByClassName("addTask-value")[0];
 
+//削除ボタンを押した時の挙動
 const removeTask = (removeButton) => {
   const targetTask = removeButton.closest("li");
   addTaskTarget.removeChild(targetTask);
 };
 
+//完了ボタンを押したときの挙動
 const completeTask = (completeButton) => {
   const targetTask = completeButton.closest("li");
   targetTask.classList.add("isComplete");
   targetTask.removeChild(completeButton);
 };
 
+//タスクを表示するための部材の情報
 const addTask = (task) => {
   const listItem = document.createElement("li");
   const removeButton = document.createElement("button");
@@ -42,6 +46,7 @@ const addTask = (task) => {
   addTaskTarget.appendChild(listItem);
 };
 
+//タスクをフォームに入力し保存ボタンを押したときの挙動
 addTaskTrigger.addEventListener("click", (event) => {
   const task = addTaskValue.value;
   addTask(task);
